7579971f9e80cd8641444a735bd923c66870fad7
JuanF1984 <juanferreyra2684@gmail.com>
2024-05-07 15:44:07 -0300

    Modificaciones en Scripts - Se elimina primer prueba JS de carrousel

--- scripts/carrousel.js
+++ scripts/carrousel.js
@@ -1,32 +1,49 @@
 const grande = document.querySelector('.grande')
 const punto = document.querySelectorAll('.punto')
-let imagenes = ["assets/img/img1.jpg", "assets/img/img2.jpg","assets/img/img3.jpg"]
+let imagenes = {
+    0:{ 
+        "src":"assets/img/img1.jpg",
+        "title":"Imagen del logo de la EES 8",
+        "alt":"Imagen del logo de la EES 8"},
+    1:{
+        "src":"assets/img/img2.jpg",
+        "title": "Estudiantes tocando instrumentos",
+        "alt": "Estudiantes tocando instrumentos"},
+    2:{
+        "src":"assets/img/img3.jpg",
+        "title": "Estudiantes leyendo con docente",
+        "alt": "Estudiantes leyendo con docente"}
+    }
+    
 let intervalo = null;
 
 
 // FUNCIÓN QUE HACE QUE SE RECORRA EL CARROUSEL DE FORMA AUTOMATICA
 // Tiene un parametro que indica en la posición que debe iniciar
 const start = (position) => {
-    let operation = 0
     intervalo = setInterval(function () {
-        // Modifica la posición, en este caso corresponde a 3 imágenes
-        operation = position * -33.3
-        // Traslada el div con las imágenes posicionando a la que corresponde
-        grande.style.transform = `translateX(${operation}%)`
-        // Recorre cada uno de los botones y le saca la clase activo
-        punto.forEach((cadaPunto, i) => {
-           punto[i].classList.remove('activo')
-        })
-        // Añadir la clase activo en el punto que hemos hecho CLICK
-        punto[position].classList.add('activo')
-        // Si llegamos a la última posición vuelve a cero. Este caso es para tres imágenes
+        insertarImagen(position)
         position >= 2 ? (position = 0) : (position++)
     }, 3500)
 }
+
 // FUNCIÓN QUE DETIENE LA REPRODUCCIÓN AUTOMÁTICA
 const stop = () => {
     clearInterval(intervalo);
 };
+
+const insertarImagen = (position) => {
+    // se agregan los distintos atributos a la etiqueta imagen del carrousel
+    document.imagen.src = imagenes[position].src
+    document.imagen.title=imagenes[position].title
+    document.imagen.alt=imagenes[position].alt
+    punto.forEach((cadaPunto, i) => {
+        // Quitamos la clase ACTIVO a TODOS los punto
+        punto[i].classList.remove('activo')
+    })
+    // se le agrega la clase ACTIVO al botón que representa al lugar de la imagen correspondiente
+    punto[position].classList.add('activo')
+}
 
 // FUNCIÓN PARA MOVER LAS IMÁGENES DESDE LOS BOTONES
 // Recorrer TODOS los punto
@@ -34,23 +51,13 @@ punto.forEach((cadaPunto, i) => {
     punto[i].addEventListener('click', () => {
         // Al hacer click lo primero que hace es detener la reproducción automática
         stop()
-        // Guardar la posición de ese PUNTO
-        let position = i
-        // Calculando el espacio que debe DESPLAZARSE el GRANDE
-        let operation = position * -33.3
-        // MOVEMOS el grande
-        grande.style.transform = `translateX(${operation}%)`
-        // Recorremos TODOS los punto
-        punto.forEach((cadaPunto, i) => {
-            // Quitamos la clase ACTIVO a TODOS los punto
-            punto[i].classList.remove('activo')
-        })
-        // Añadir la clase activo en el punto que hemos hecho CLICK
-        punto[i].classList.add('activo')
-        //Una vez que ya se detuvo la reproducción automática para ver la imagen,
-        // vuelve a reproducirse indicando desde que imágen debe hacerlo 
+        // Se llama a la función para que ponga imagen en la etiqueta correspondiente
+        insertarImagen(i)
+        // Se llama nuevamente a la reproducción automática de imagenes
         start(i)
     })
 })
 
-start(0)
+// iniciamos la reproducción de imagenes en la segunda imagen porque inicialmente inicia en la primera 
+// se carga junto con la página la primer página antes de que inicie la reproducción automática
+start(1)
