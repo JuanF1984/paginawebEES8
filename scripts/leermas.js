c144d01a8866e68a2f0eaf7f17254bb9891f052a
SanttFunes <santixfunesx@gmail.com>
2024-06-04 23:19:16 +0200

    agregue pagina perfil docente

--- scripts/leermas.js
+++ scripts/leermas.js
@@ -1,12 +1,15 @@
-const perfilMasMenos = document.getElementById('perfilMasMenos')
+const perfilMasMenos = document.getElementById('perfilMasMenos');
 const perfilTextHidden = document.querySelector('.perfilTextHidden');
+const perfilText = document.querySelector('.perfilText');
 
 perfilMasMenos.addEventListener('click', () => {
     if (perfilMasMenos.textContent === 'Leer Más') {
         perfilTextHidden.classList.add('perfilTextVisible')
+        perfilText.classList.add('perfilTextHidden')
         perfilMasMenos.textContent = 'Leer Menos'
     }else{
         perfilTextHidden.classList.remove('perfilTextVisible')
+        perfilText.classList.remove('perfilTextHidden')
         perfilMasMenos.textContent = 'Leer Más'
     }
 })
