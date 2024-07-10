9559210fcea69e8a355ad24a6cdf4bf6d04abf68
JuanF1984 <juanferreyra2684@gmail.com>
2024-07-10 19:08:31 -0300

    Se agrega efecto load

---
+++ scripts/loader.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let loader = document.getElementById("loaderContainer");
+    let content = document.getElementById("bodyContainer");
+    loader.style.display = "none";
+    content.style.display = "block";
+});
